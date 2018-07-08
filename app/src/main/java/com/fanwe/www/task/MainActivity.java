e28d9be808f639c4bef4eede8cfb371eda7af288
zjun <565061763@qq.com>
2018-07-08 19:36:19 +0800

    demo

--- app/src/main/java/com/fanwe/www/task/MainActivity.java
+++ app/src/main/java/com/fanwe/www/task/MainActivity.java
@@ -8,10 +8,9 @@ import android.view.View;
 import com.fanwe.lib.task.FTask;
 import com.fanwe.lib.task.FTaskManager;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     public static final String TAG = MainActivity.class.getSimpleName();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,7 +19,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
-    public void onClickAddTask(View view)
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn_add_task:
+                addTask();
+                break;
+            case R.id.btn_cancel_task:
+                FTaskManager.getInstance().cancelTag(TAG, true);
+                break;
+        }
+    }
+
+    private void addTask()
     {
         new FTask(TAG)
         {
@@ -65,10 +78,5 @@ public class MainActivity extends AppCompatActivity
                 Log.i(TAG, "onFinally");
             }
         }.submit();
-    }
-
-    public void onClickCancelTask(View view)
-    {
-        FTaskManager.getInstance().cancelTag(TAG, true);
     }
 }
